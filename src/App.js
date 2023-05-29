bab0a9c29287b6fa629aa31e1dca77f3bfdd7d34
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-05-28 21:28:30 -0300

    testando run build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { Switch, Route, Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
 import './App.css';
 import View from './pages/View';
@@ -7,9 +7,10 @@ import View from './pages/View';
 function App() {
   return (
     <div className="App">
-      Poker Counter App
+      Poker Counter App - hehe
       <Switch>
-        <Route exact path="/" component={View} ></Route>
+        <Route exact path="/poker-counter" component={View} ></Route>
+        <Redirect to='/poker-counter' />
       </Switch>
 
     </div>
